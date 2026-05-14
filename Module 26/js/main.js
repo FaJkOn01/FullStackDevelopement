3261923a54ce13e844b8831c91cf73dc17cb95b0
FaJkOn01 <fajkonbytyqi10@gmail.com>
2026-05-14 20:44:53 +0100

    mosule 26 part2

--- Module 26/js/main.js
+++ Module 26/js/main.js
@@ -1,8 +1,8 @@
-console.log('GoodBye!');
+//console.log('GoodBye!');
 
-var x = 10;
+//var x = 10;
 
-var _value = "Digital School";
+//var _value = "Digital School";
 
 //var 123 = 55; //wrong way
 //var *value = 56; //wrong way
@@ -25,3 +25,101 @@ var _value = "Digital School";
 // After the first letter we can use digits (0 to 9), for example, value1.
 
 //  JavaScript variables are case sensitive, for example, x and X are different variables.
+
+
+
+//var a= 50 + 50;
+//console.log(a);
+
+//var b = 20;
+//var c = 30;
+
+
+//var d = b + c;
+
+//console.log(d);
+
+
+//var s = 32;
+//var k = 44;
+//var h = s * k;
+//console.log(h);
+
+//var a = 99;
+//a++;
+//console.log(a);
+//a--;
+//console.log(a);
+
+
+
+//var x = 15;
+//x+=5;
+//console.log(x);
+
+//var x = 5;
+//var y = 5;
+//console.log(x == y);
+
+//console.log(x != y);
+
+//var firstName = "Digital";
+//var lastName = "School";
+//var fullName = firstName + " " + lastName;
+//console.log(fullName);
+
+//var school = "Our School";
+//console.log(`Hello from ${school}`);
+
+//var klub = "SHF ZIDANE";
+//console.log(`Hello from ${klub}`)
+
+//var age = 24;
+//console.log(age >= 18 && age <= 65);
+
+//var var1= 5;
+//var var2 = 6;
+//console.log(var1 == 5 || var2 == 5);
+
+//var var4 = 9;
+//var var5 = 10;
+//console.log(!(var4 == var5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var shkollafutbollit = "SHF ZIDANE";
+console.log(`Pershendetje nga ${shkollafutbollit}`)
+
+var var1 = 10;
+var var2 = 20;
+var var4 = var1 * var2;
+console.log(`Prodhimi i var1 dhe var2 eshte: ${var4}`);
+
+var var5 = 100;
+var5++;
+console.log(var5);
