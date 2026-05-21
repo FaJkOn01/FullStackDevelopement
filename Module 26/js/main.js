c255cca7f1e24295353db93b36af9b922c554e52
FaJkOn01 <fajkonbytyqi10@gmail.com>
2026-05-21 20:43:03 +0100

    module27

--- Module 26/js/main.js
+++ Module 26/js/main.js
@@ -112,7 +112,7 @@
 
 
 
-var shkollafutbollit = "SHF ZIDANE";
+/*var shkollafutbollit = "SHF ZIDANE";
 console.log(`Pershendetje nga ${shkollafutbollit}`)
 
 var var1 = 10;
@@ -123,3 +123,85 @@ console.log(`Prodhimi i var1 dhe var2 eshte: ${var4}`);
 var var5 = 100;
 var5++;
 console.log(var5);
+
+
+
+
+
+
+function toCelsius(Fahrenheight){
+    return ( 5 / 9)* (Fahrenheight-32 );
+
+}
+
+
+var result = toCelsius(54)
+console.log("Fahrenheight in celsius is "+result+" degrees")
+*/
+
+function digitalschool(){
+    var shkolla = ' DigitalSchool';
+    alert(shkolla)
+}
+
+digitalschool()
+
+function siperfaqja_e_trekendeshit(baza, lartesia){
+    return baza * lartesia * 0.5
+}
+console.log('Siperfaqja e trekendeshit eshte: ' + siperfaqja_e_trekendeshit(5, 7) + ' cm2')
+
+var audia = {
+    type: "A6",
+    color: "white",
+    viti_i_prodhimit: 2016,
+    kilometra: 220. 000,
+    karburanti: 'Disel',
+    engine:function(){
+        alert('Vroooom')
+    },
+    get getkilometra(){
+        return this.kilometra
+    },
+    set setkilometra(km){
+        this.kilometra = km;
+    }
+
+}
+
+alert(audia.type)
+
+var grupi_7 = {
+    niveli : 'Web ADV',
+    ora: '19:30',
+    nr_nx : '14'
+
+
+}
+
+alert(grupi_7.nr_nx)
+
+var klasa_7_2 = {
+    nr_nx: '24',
+    djem : '16',
+    kati : '3',
+    kujdestari:function(){
+        alert('Edon Kelmendi')
+    }
+
+}
+
+alert(klasa_7_2.djem)
+
+audia.engine()
+
+klasa_7_2.kujdestari()
+console.log(audia.getkilometra)
+audia.setkilometra = 120;
+console.log(audia.getkilometra)
+
+
+
+
+
+
